18eb1224eb901cbcfa38b15061bd3f0931f5f5da
onpon4 <onpon4@riseup.net>
2016-12-15 18:44:44 -0500

    1.9.1 release.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ your game's levels.
 """.strip()
 
 setup(name="tmx",
-      version="1.9.1a0",
+      version="1.9.1",
       description="Simple TMX library.",
       long_description=long_description,
       author="Julie Marchant",
--- tmx/__init__.py
+++ tmx/__init__.py
@@ -37,7 +37,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 
-__version__ = "1.9.1a0"
+__version__ = "1.9.1"
 
 
 import os
