6697d317dd237f1318f2e920b07b7ed142a2a86b
Layla Marchant <diligentcircle@riseup.net>
2020-04-24 18:46:47 -0400

    One more

--- tmx/__init__.py
+++ tmx/__init__.py
@@ -1,5 +1,4 @@
 # Simple TMX library
-# Copyright (c) 2014-2016 Julie Marchant <onpon4@riseup.net>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
