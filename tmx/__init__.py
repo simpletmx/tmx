441d1cf8f9a4384791d4c73dc60a172e01eda4c7
Layla Marchant <diligentcircle@riseup.net>
2020-04-25 15:33:22 -0400

    Updated version number

--- tmx/__init__.py
+++ tmx/__init__.py
@@ -30,13 +30,8 @@ TMX format specification, found here:
 http://doc.mapeditor.org/en/latest/reference/tmx-map-format/
 """
 
-from __future__ import division
-from __future__ import absolute_import
-from __future__ import print_function
-from __future__ import unicode_literals
 
-
-__version__ = "1.11a0"
+__version__ = "1.13a0"
 
 
 import os
